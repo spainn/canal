1430286cfddd5c37cd40c0da2b1d32479d66a996
spainn <spainn@users.noreply.github.com>
2025-01-15 23:52:10 -0600

    Cleaned up nutrient.py and product.py

--- nutrient.py
+++ nutrient.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Nutrient:
     name: str
-    #percentDailyValue: int
     unitName: str
     value: float
-
+    
+    # verify value is a float
     def __post_init__(self):
         if isinstance(self.value, str):
             object.__setattr__(self, 'value', float(self.value))
--- product.py
+++ product.py
@@ -15,28 +15,14 @@ class Product:
 
     brandName: str
     description: str
-    serving_size: float             # g or mL amount
-    servingSizeUnit: str           # g or mL
-    nutrients: tuple[Nutrient, ...] # per 100g or 100mL
+    serving_size: float             # g or mLt amount
+    servingSizeUnit: str            # g or mLt
+    nutrients: tuple[Nutrient, ...] # per 100g or 100mLt
     
     def __post_init__(self):
         if isinstance(self.serving_size, str):
             object.__setattr__(self, 'serving_size', float(self.serving_size))
-
-#    @property
-#    def nutrients_per_serving(self):
-#        """Returns a Dict[str, Nutrient] of each nutrient's
-#        name and value per serving rather than per 100g."""
-#        
-#        nutrients_per_serving = []
-#        for n in self.nutrients:
-#            new_nutrient = Nutrient(name=n.name, 
-#                                    unitName=n.unitName,
-#                                    value=n.value*(self.serving_size/100))
-#        nutrients_per_serving.append(new_nutrient)
-#
-#        return nutrients_per_serving
-
+    
     def get_macros_from_units(self, units):
         macros = dict()   
 
@@ -50,9 +36,4 @@ class Product:
         grams = servings*self.serving_size
 
         return self.get_macros_from_grams(grams=grams)
-"""
-nutrientName
-percentDailyValue
-unitName
-valuei
-"""
+
